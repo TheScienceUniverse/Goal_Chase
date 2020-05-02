0763498251ff347a186ce816619fe29a21cdddd8
SayanThe01Guy <Mr.S123@sDev>
2020-05-02 10:33:09 +0530

    Create menu in main activity

--- app/src/main/java/com/example/goal_chase/MainActivity.java
+++ app/src/main/java/com/example/goal_chase/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.goal_chase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Utility utility = new Utility(this);
+		utility.prepareFile();
+
 		FloatingActionButton fab = findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -22,5 +29,28 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(new Intent(MainActivity.this, EditActivity.class));
 			}
 		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.refresh:
+				Toast.makeText(this, "Refreshed", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.copy:
+				Toast.makeText(this, "Copied", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.download:
+				Toast.makeText(this, "Downloaded", Toast.LENGTH_SHORT).show();
+				break;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
